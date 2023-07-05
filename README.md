941ac049a1c5d4c231ae8a501cb252f6e0cd6907
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-07-04 20:18:07 -0500

    Finished basic functionality

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Readme-Gen
+# README-Gen
+
+## Table of Contents
+ - [Description](#description)
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [Contributing](#contributing)
+ - [Testing](#testing)
+ - [Questions](#questions)
+ - [License](#license)
+
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## License
+
+## Description
+I wanted to make a program that could generate README in Markdown easily.
+
+## Installation
+Run node index.js at the root directory
+
+## Usage
+Follow the prompts and fill in the sections. When you're finished the file will be neatly generated for you. If you would like to omit a section, just press enter instead of entering text.
+
+## Contributing
+Be happy
+
+## Testing
+Write a prompt tester with Jest
+
+## Questions
+FAQ
+
+MIT License
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- index.js
+++ index.js
@@ -1,5 +1,107 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const licenses = {
+  MIT: {
+    badge: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    text: `MIT License
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`,
+  },
+  GNU: {
+    badge: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+    text: `GNU General Public License (GPL) version 3
+
+    This is a free software license, compatible with version 3 of the GNU General Public License (GPL).
+
+    For more information about this license, please visit the [GNU website](https://www.gnu.org/licenses/gpl-3.0).`,
+  },
+  wtfpl: {
+    badge: "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/)",
+    text: `WTFPL – Do What the Fuck You Want to Public License
+
+    This is a permissive license that allows you to do whatever you want with the code, without any restrictions.
+
+    For more information about this license, please visit the [WTFPL website](http://www.wtfpl.net/).`,
+  },
+  Apache: {
+    badge: "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    text: `Apache License 2.0
+
+    This is a permissive open-source license that allows you to use, copy, modify, distribute, and sublicense the code.
+
+    For more information about this license, please visit the [Apache website](https://opensource.org/licenses/Apache-2.0).`,
+  },
+  ISC: {
+    badge: "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
+    text: `ISC License
+
+    This is a permissive open-source license that allows you to use, copy, modify, and distribute the code, with or without modifications.
+
+    For more information about this license, please visit the [ISC website](https://opensource.org/licenses/ISC).`,
+  },
+  MPL: {
+    badge: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+    text: `Mozilla Public License 2.0
+
+    This is a copyleft license that allows you to use, modify, distribute, and sublicense the code, as long as any modifications are made available under the same license.
+
+    For more information about this license, please visit the [Mozilla website](https://opensource.org/licenses/MPL-2.0).`,
+  },
+  BSD: {
+    badge: "[![License: BSD 3-Clause](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+    text: `BSD 3-Clause License
+
+    This is a permissive open-source license that allows you to use, modify, distribute, and sublicense the code.
+
+    For more information about this license, please visit the [BSD website](https://opensource.org/licenses/BSD-3-Clause).`,
+  },
+  Unlicense: {
+    badge: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
+    text: `The Unlicense
+
+    This is a public domain dedication that allows you to use the code without any restrictions.
+
+    For more information about this license, please visit the [Unlicense website](http://unlicense.org/).`,
+  },
+};
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function objToString(obj) {
+  var content = Object.entries(obj).reduce((str, [p, val]) => {
+    if (val == "") { return str }
+    if (p === "name") {
+      const licenseBadge = licenses[obj.license[0]].badge;
+      return `# ${capitalizeFirstLetter(val)}\n\n${generateTableOfContents(obj)}\n\n${licenseBadge}\n\n## License\n\n`;
+    }
+    if (p === "license") { return str; }
+    return `${str}## ${capitalizeFirstLetter(p)}\n${val}\n\n`;
+  }, '');
+
+  const licenseText = licenses[obj.license[0]].text.replace(/^\s+|\s+$/g, "");
+  content += licenseText + "\n";
+
+  return content;
+}
+
+
+
+
+function generateTableOfContents(obj) {
+  var table = Object.entries(obj).reduce((str, [p, val]) => {
+    if (val == "" || p === "name") { return str }
+    return `${str} - [${capitalizeFirstLetter(p)}](#${p.toLowerCase()})\n`;
+  }, '');
+  return "## Table of Contents\n" + table;
+}
+
 inquirer
   .prompt([
     {
@@ -24,7 +126,7 @@ inquirer
     },
     {
       type: 'input',
-      name: 'contribution',
+      name: 'contributing',
       message: 'Tell people how they can contribute.',
     },
     {
@@ -41,15 +143,13 @@ inquirer
       type: 'checkbox',
       message: 'What is your preferred method of communication?',
       name: 'license',
-      choices: ['MIT', 'GNU', 'wtfpl'],
+      choices: ['MIT', 'GNU', 'wtfpl', 'Apache', 'ISC', 'MPL', 'BSD', 'Unlicense'],
     },
   ])
   .then((data) => {
-    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
-    const title = `#${data.name}`;
-    const file = `${title}\n##Description\n\n${data.description}\n\n##Table of Contents\n\n- [Usage](#usage)\n- [Credits](#credits)\n- [License](#license)\n- [Features](#features)\n\n#Installation\n\n${data.installation}\n\n#Usage\n\n${data.usage}\n\n#Contribution\n\n${data.contribution}\n\n#Testing\n\n${data.testing}\n\n#Questions\n\n${data.questions}\n\n#License\n\n${data.license}\n\n`.trim();
-    console.log(file);
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+    const filename = `output\\README.md`;
+    const fileData = objToString(data);
+    fs.writeFile(filename, fileData, (err) =>
       err ? console.log(err) : console.log('Success!')
     );
   });
